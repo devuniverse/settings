aa628b2efbad39544d782f365cda990d0b69e52f
Janvier <janviersoft@gmail.com>
2019-03-09 20:50:28 -0500

    Updated config file

--- src/Config/profiles.php
+++ src/Config/profiles.php
@@ -1,17 +1,6 @@
 <?php
 return [
-  /**
-   *|
-   */
-  "profiles_default_disk" => "public",
-  /**
-   *|
-   */
-  "files_upload_path"       => "uploads",
-  /**
-   *|
-   */
-  "files_upload_thumb_path" => "uploads/thumbnails",
+
   /**
    *|
    */
@@ -24,27 +13,6 @@ return [
    *|
    */
   "master_file_extend" => "profiles::main",
-  /**
-   *|
-   */
-  "files_per_page" => 25,
-
-  /**
-  *|  #1  : defaults to public. Like Laravel
-  *|  #2  : File systems in this setting MUST be mutually exclusive.
-  *|        That is, Only one can have the default value to true
-  *|
-   */
-  "profiles_storage_disk" => [
-
-      "public"  => [
-
-      ],
-      "s3" => [
-        "cname_path" => "", //  s3.amazonaws.com/bucket
-        "cname_url"  => "",  //  bucket.s3.amazonaws.com
-      ]
-    ],
 
   // "default_file_system" => "public",
   /**
@@ -62,13 +30,6 @@ return [
       'jquery'   => true,
       'animate' => true,
       'fontawesome' => true
-  ],
-
-  /**
-   *|
-   */
-  'separate_uploads' => [
-
   ],
   'override_default' => false
 ];
